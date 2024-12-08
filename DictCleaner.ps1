9298a8dd48393492c756cdf83090a2fb06a5b27f
Pokesalot <athankinson@gmail.com>
2024-12-07 23:06:34 -0500

    Initial Dictionary Commit

---
+++ DictCleaner.ps1
@@ -0,0 +1,40 @@
+﻿cd $PSScriptRoot
+
+$fullDictionary = gc .\RawDictWithDefinitions.txt
+
+$words = @()
+$fullDictionary[1..($fullDictionary.Length-1)] | %{
+    $words += ($_ -split "`t")[0]
+}
+$words | Out-File "AllWords.txt"
+
+#Trim the dictionary to not include non-english words
+$words = @()
+
+$trimPhrases = @(
+    "(Hindi)","(Zulu)","(Russian)","(Hawaiian)","(Japanese)","(Slovene)","(Maori)","(French)","(Irish)","(Scots)","(Shakespeare)"
+    ,"(Dutch)","(archaic)","(Latin)","(Spenser)","(dialect)","(Australian slang)","(Welsh)","(Spanish)","(Korean)","(Greek)","(Italian)"
+    ,"(German)","(obsolete)"
+)
+$trims = @{}
+$trimPhrases | %{$trims[$_] = @()}
+
+$fullDictionary[1..($fullDictionary.Length-1)] | %{ #($fullDictionary.Length-1)
+    $line = $_
+    $GetsTrimmed = $false
+    $trimPhrases | %{
+        if($line -match $_){
+            $trims[$_] += $line
+            $GetsTrimmed = $true
+        }
+    }
+
+    if(-not $GetsTrimmed){
+        $words += $line
+    }
+}
+
+$words | Out-File "TrimmedDictionary.txt"
+$trimPhrases | %{
+    $trims[$_] | Out-File ".\Removed Words\$_.txt"
+}
---
+++ DictTreeBuilder.ps1
@@ -0,0 +1,54 @@
+﻿param ($inputFile = $null, $outputFile = "DictionaryTree.json")
+
+cd $PSScriptRoot
+$Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+$dict = @{
+    "" = @(3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)
+    #The only 1-letter words are A and I
+    #Values are bitwise representations
+        #MSB is whether the current last letter makes a word
+        #LSB is whether there exist words starting with the last letter
+
+        #e.g. at $dict["wor"] the 's' position will be 1 (not a word, but the start of "worse")
+                             #the 'd' position will be 3 (word is a word, and the start of "words")
+                             #the 'a' position will be 0 (not a word or the start of one)
+}
+
+function AddWordToDict($ArrayOfLetters){
+    0..($ArrayOfLetters.Count -2) | %{
+
+        if(-not $dict.ContainsKey($($ArrayOfLetters[0..$_] -join ""))){
+            $dict[$($ArrayOfLetters[0..$_] -join "")] = @(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        }
+
+        if($_ -eq $ArrayOfLetters.Count -2){
+            $dict[$($ArrayOfLetters[0..$_] -join "")][$Letters.IndexOf($ArrayOfLetters[$_+1])] = $dict[$($ArrayOfLetters[0..$_] -join "")][$Letters.IndexOf($ArrayOfLetters[$_+1])] -bor 2
+        }else{
+            $dict[$($ArrayOfLetters[0..$_] -join "")][$Letters.IndexOf($ArrayOfLetters[$_+1])] = $dict[$($ArrayOfLetters[0..$_] -join "")][$Letters.IndexOf($ArrayOfLetters[$_+1])] -bor 1
+        }
+    }
+
+}
+
+if($inputFile -eq $null){
+    Write-Host "Entering words manually. Enter a blank word to finish"
+    $word = (Read-Host "Enter a valid word").ToUpper() -split "" | ?{$_ -ne ""}
+    while($word -ne $null){
+        AddWordToDict $word
+
+        $word = (Read-Host "Enter a valid word").ToUpper() -split "" | ?{$_ -ne ""}
+    }
+}else{
+    $words = gc $inputFile
+    $words | %{AddWordToDict $($_.ToUpper() -split "" | ?{$_ -ne ""}) }
+}
+
+$dictLines = $($dict | ConvertTo-Json -Depth 15 -Compress) -replace ",`"","`n`"" -replace "{","{`n" -replace "}","`n}" -split "`n"
+$dictLines = $($dictLines[1..($dictLines.Count-2)] | Sort-Object)
+
+$writeString = "{`n"
+
+$dictLines[0..($dictLines.Count-2)] | %{ $writeString += $($_ -replace ":"," : ") + ",`n`t"}
+$writeString += $($dictLines[$dictLines.Count-1] + "`n}")
+$writeString | Out-File $outputFile
